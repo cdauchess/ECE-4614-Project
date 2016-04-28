2b4fa41d8067368c89ae3d6e6f3bf018cc4c808c
cdauchess <dauchess@vt.edu>
2016-04-28 01:10:45 -0400

    Macro Settings Layout
    Added text fields and a button to the macro settings layout.  Will allow
    for modification of the macro names and text

--- 4614_App_Project/app/src/main/java/com/example/charlie/a4614app/MacroSettings.java
+++ 4614_App_Project/app/src/main/java/com/example/charlie/a4614app/MacroSettings.java
@@ -16,15 +16,19 @@ public class MacroSettings extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+/*        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void SaveMacros(View view){
+        
     }
 
 }
